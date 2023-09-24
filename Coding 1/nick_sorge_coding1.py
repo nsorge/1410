62bb0642bd167955b80d79341ffdbb123777583f
sorge <nicksorge25@gmail.com>
2023-09-23 18:29:15 -0600

    FOR REAL FINAL

--- Coding 1/nick_sorge_coding1.py
+++ Coding 1/nick_sorge_coding1.py
@@ -1,25 +1,22 @@
 '''Permutations in l33t speak!'''
 
-# Collection of common l33t corrections
+# Collection of common "l33t" corrections
 l33t_dict = {'a':'4',
              'e':'3',
              'i':'1',
              'o':'0'}
 
 
-def get_word():
-    global input_word
-    while True:
-        # Input word
-        input_word = input("Enter a word: ")
-        # Converts the input string to a list of characters
-        input_word = list(input_word)
-        # Checks input word for repeating characters
-        for i, val in enumerate(input_word):
-            if val in input_word:
-                print('Try a word that does not repeat characters.')
-                break
-        return False
+def check_repeat(word):
+    '''Checks that there are no repeated characters'''
+    char = []
+    for i, val in enumerate(word):
+        # Checks if the current letter being evaluated is already in char list
+        if val in char:
+            return True
+        # Adds letter to char list
+        char.append(word[i])
+    return False
 
 
 def permutations(word, start=0):
@@ -38,19 +35,36 @@ def permutations(word, start=0):
 
 
 def l33t_speak(word):
-    '''Creative Element: Converts normal characters to l33t speak'''
-    # Enumerates over the characters
+    '''Creative Element: Converts normal characters to "l33t speak"'''
     for i, val in enumerate(word):
+        # Changes character to l33t correction if applicable
         if val in l33t_dict:
-            # Changes character to l33t correction
             word[i] = l33t_dict[word[i]]
 
 def main():
-    get_word()
+    '''Main'''
+    while True:
+        # Prompts user for input word
+        input_word = input("Enter a word: ")
+        # Checks input word is all letters
+        if input_word.isalpha():
+            pass
+        else:
+            print('Only use letters in your input.')
+            continue
+        # Converts the input string to a list of characters
+        input_word = list(input_word)
+        # Checks input word for repeating characters
+        if check_repeat(input_word):
+            print('Try a word that does not repeat letters.')
+            continue
+        else:
+            break
     l33t_speak(input_word)
     permutations(input_word)
 
-main()
+if __name__ == '__main__':
+    main()
 
 # Thoughts
 
@@ -59,5 +73,8 @@ main()
 #head around. For the creative element I used a dictionary to make key and value pairs of
 #commmon "l33t speak" changes. the l33t_speak function enumerates the input_word list and
 #compares each character with the keys in the dictionary and makes the change if applicable.
+#I could not figure out how to make the try except format work because having numbers or
+#repeating characters did not create a python error code I could use to except the try so
+#I used if else statements instead. 
 
 #Use the word "haxor" for a good example.
